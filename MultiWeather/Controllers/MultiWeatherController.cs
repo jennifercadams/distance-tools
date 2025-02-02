8e7dda0dfb0b7c18c993d4933dec0ad8520dbef5
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-01 20:08:35 -0500

    Implement controller with GetCurrent endpoint

---
+++ MultiWeather/Controllers/MultiWeatherController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiWeather.Services;
+using System.Text.Json;
+
+namespace MultiWeather.Controllers
+{
+    [ApiController]
+    [Route("MultiWeather")]
+    public class MultiWeatherController : ControllerBase
+    {
+        private readonly WeatherApiService _weatherApiService;
+
+        public MultiWeatherController()
+        {
+            _weatherApiService = new WeatherApiService();
+        }
+
+        [HttpGet]
+        [Route("GetCurrent")]
+        public async Task<ContentResult> GetCurrent(string locationQuery)
+        {
+            var response = await _weatherApiService.GetCurrentAsync(locationQuery);
+            var jsonResponse = JsonSerializer.Serialize(response);
+            return new ContentResult()
+            {
+                StatusCode = StatusCodes.Status200OK,
+                Content = jsonResponse,
+                ContentType = "application/json"
+            };
+        }
+
+    }
+}
