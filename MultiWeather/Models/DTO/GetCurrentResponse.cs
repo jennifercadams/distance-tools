3d85b9c658cc0f1ef725ee0f48c4adef0498b15f
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-01 20:07:22 -0500

    Implement WeatherApiService

---
+++ MultiWeather/Models/DTO/GetCurrentResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiWeather.Models.DTO
+{
+    public class GetCurrentResponse
+    {
+        public string? LocationName { get; set; }
+        public string? TimeZone { get; set; }
+        public string? ConditionText { get; set; }
+        public string? ConditionIcon { get; set; }
+        public string? Temperature { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ MultiWeather/Services/WeatherApiService.cs
@@ -0,0 +1,51 @@
+﻿using MultiWeather.Models.DTO;
+using MultiWeather.Models.WeatherApi;
+using System.Text.Json;
+
+namespace MultiWeather.Services
+{
+    public class WeatherApiService
+    {
+        private const string BaseUrl = "https://api.weatherapi.com/v1/";
+
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public WeatherApiService()
+        {
+            _httpClient = new HttpClient { BaseAddress = new Uri(BaseUrl) };
+            _apiKey = Environment.GetEnvironmentVariable("WEATHER_API_KEY") ?? "";
+        }
+
+        public async Task<GetCurrentResponse> GetCurrentAsync(string locationQuery)
+        {
+            var path = $"current.json?key={_apiKey}&q={locationQuery}";
+
+            try
+            {
+                var response = await _httpClient.GetAsync(path);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var currentResponse = JsonSerializer.Deserialize<CurrentResponse>(responseBody);
+
+                if (currentResponse == null)
+                    throw new Exception();
+
+                var getCurrentResponse = new GetCurrentResponse()
+                {
+                    LocationName = currentResponse.Location.Name,
+                    TimeZone = currentResponse.Location.TimeZone,
+                    ConditionText = currentResponse.Current.Condition.Text,
+                    ConditionIcon = currentResponse.Current.Condition.Icon,
+                    Temperature = $"{currentResponse.Current.TempC}° C | {currentResponse.Current.TempF}° F"
+                };
+
+                return getCurrentResponse;
+            }
+            catch (Exception ex)
+            {
+                return new GetCurrentResponse() { Error = ex.Message };
+            }
+        }
+
+    }
+}
