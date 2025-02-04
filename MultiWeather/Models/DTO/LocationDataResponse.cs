ad64cd3105e9ff3cafc2762dc663b6bde0dc9125
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-03 19:38:48 -0500

    Default string properties in LocationDataResponse to empty strings

--- MultiWeather/Models/DTO/LocationDataResponse.cs
+++ MultiWeather/Models/DTO/LocationDataResponse.cs
@@ -2,9 +2,9 @@
 {
     public class LocationDataResponse
     {
-        public string PlaceName { get; set; }
-        public string CountryCode { get; set; }
-        public string PostalCode { get; set; }
+        public string PlaceName { get; set; } = "";
+        public string CountryCode { get; set; } = "";
+        public string PostalCode { get; set; } = "";
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
     }
