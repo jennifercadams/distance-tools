59c71224da7b4cd8037ad64417163b99ca47ee14
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-06 18:34:00 -0500

    Remove location data service

--- MultiWeather/Models/LocationData/Country.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MultiWeather.Models.LocationData
-{
-    public class Country(string countryCode, string countryName)
-    {
-        public string CountryCode { get; private set; } = countryCode;
-        public string CountryName { get; private set; } = countryName;
-        public List<PostalCodeLocation> Locations { get; private set; } = [];
-    }
-}
