a47dc7f162214252f24e1c1a4b00a4140a5da268
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-09 10:12:57 -0500

    Add logic to include swagger dependencies only in development environment

--- MultiWeather/Program.cs
+++ MultiWeather/Program.cs
@@ -11,9 +11,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSingleton<WeatherApiCache>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Add Swagger dependencies in development
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
 
